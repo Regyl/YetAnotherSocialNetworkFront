27f6b50ed7219a51ddef1b9ba95a3fa999d5a997
Regyl <novikovevgeny1@gmail.com>
2021-09-30 23:20:01 +0300

    add main page

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,37 +1,71 @@
 import * as React from 'react';
-import {Box, Toolbar, IconButton, Typography, Button, AppBar, MenuItem, Menu} from "@material-ui/core";
+import {Box, Toolbar, IconButton, Typography, Button, AppBar, MenuItem, Menu, makeStyles} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import {AccountCircle} from "@material-ui/icons";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        [
+            theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+}));
+
 
 export default function MainPage() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
-                        size="large"
-                        edge="start"
+                        className={classes.menuButton}
                         color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        Testing System
                     </Typography>
-
                     <IconButton
-                        size="large"
-                        edge="end"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
+                        id="basic-button"
+                        aria-controls="basic-menu"
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                        >
+                        <AccountCircle/>
                     </IconButton>
+                        <Menu
+                            id="basic-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{
+                                'aria-labelledby': 'basic-button',
+                            }}
+                        >
+                            <MenuItem onClick={handleClose}>Sign in</MenuItem>
+                            <MenuItem onClick={handleClose}>Sign up</MenuItem>
+                        </Menu>
                 </Toolbar>
             </AppBar>
         </Box>
