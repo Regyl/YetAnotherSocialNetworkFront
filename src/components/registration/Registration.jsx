0c8d509879f534ef2ce441fa01435261e328f8e5
Regyl <novikovevgeny1@gmail.com>
2021-10-02 22:40:58 +0300

    Add flip cards

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -2,31 +2,33 @@ import React from 'react';
 import {
     Grid, Button,
     Card, CardActions, CardMedia,
-    Typography
+    Typography, TextField
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import AdminImage from '../../img/admin.jpg'
 import ProfessorImage from '../../img/professor.jpg'
 import StudentImage from '../../img/student.jpg'
+import ReactCardFlip from "react-card-flip";
+import {Link} from "react-router-dom";
 
 const styles = {
     adminCardAction: {
         justifyContent: 'center',
         backgroundImage: `url(${AdminImage})`,
-        minHeight: 100,
+        minHeight: 165,
     },
     professorCardAction: {
         justifyContent: 'center',
         backgroundImage: `url(${ProfessorImage})`,
-        minHeight: 100,
+        minHeight: 165,
     },
     studentCardAction: {
         justifyContent: 'center',
         backgroundImage: `url(${StudentImage})`,
-        minHeight: 100,
+        minHeight: 165,
     },
     cardMedia: {
-        minHeight: 100,
+        minHeight: 165,
     },
 }
 
@@ -39,90 +41,141 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-const Registration = (props) => {
-    this.state = {
-        isFlipped: false
-    };
+class Registration extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAdminFlipped: false,
+            isProfessorFlipped: false,
+            isStudentFlipped: false
+        };
+        this.flipAdminCard = this.flipAdminCard.bind(this);
+        this.flipProfessorCard = this.flipProfessorCard.bind(this);
+        this.flipStudentCard = this.flipStudentCard.bind(this);
+    }
 
+    flipAdminCard(e) {
+        e.preventDefault();
+        this.setState(prevState => ({ isAdminFlipped: !prevState.isAdminFlipped, isProfessorFlipped: false, isStudentFlipped: false }));
+    }
+    flipProfessorCard(e) {
+        e.preventDefault();
+        this.setState(prevState => ({ isProfessorFlipped: !prevState.isProfessorFlipped, isAdminFlipped: false, isStudentFlipped: false }));
+    }
+    flipStudentCard(e) {
+        e.preventDefault();
+        this.setState(prevState => ({ isStudentFlipped: !prevState.isStudentFlipped, isAdminFlipped: false, isProfessorFlipped: false }));
+    }
 
-        return (
-            <Grid>
-                <Typography align={"center"}>
-                    Choose your champion
-                </Typography>
+        render() {
 
-                {/*> Administrator card pair*/}
-                <Grid
-                    container
-                    spacing={2}
-                    direction={ "row-reverse"}
-                    alignItems={"center"}
-                    justify={"center"}
-                >
-                    <Grid item xs={3}>
-                        <Card>
-                            <CardActions style={styles.adminCardAction}>
-                                <Button variant={"outlined"} color={"primary"}>Administrator</Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Card>
-                            <CardMedia image={AdminImage} style={styles.cardMedia}/>
-                        </Card>
-                    </Grid>
-                </Grid>
+            return (
+                <Grid>
+                    <Typography align={"center"}>
+                        Choose your champion
+                    </Typography>
 
-                {/*> Professor card pair*/}
-                <Grid
-                    container
-                    spacing={3}
-                    direction={"row"}
-                    alignItems={"center"}
-                    justify={"center"}
+                    {/*> Administrator card pair*/}
+                    <Grid
+                        container
+                        spacing={2}
+                        direction={"row-reverse"}
+                        alignItems={"center"}
+                        justify={"center"}
                     >
-                    <Grid item xs={3}>
-                        <Card>
-                            <CardActions style={styles.professorCardAction}>
-                                <Button variant={"outlined"}>Professor</Button>
-                            </CardActions>
-                        </Card>
+                        <Grid item xs={3}>
+                            <ReactCardFlip isFlipped={this.state.isAdminFlipped}>
+                                <Card>
+                                    <CardActions style={styles.adminCardAction}>
+                                        <Button variant={"outlined"} onClick={this.flipAdminCard}>Administrator</Button>
+                                    </CardActions>
+                                </Card>
+                                <Card>
+                                    <Grid container
+                                        direction={"column"}>
+                                        <TextField required label="Login"/>
+                                        <TextField required label="Password" type={"password"}/>
+                                        <TextField required label="Repeat password" type={"password"}/>
+                                        <Button>Register</Button>
+                                    </Grid>
+                                </Card>
+                            </ReactCardFlip>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Card>
+                                <CardMedia image={AdminImage} style={styles.cardMedia}/>
+                            </Card>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={3}>
-                        <Card>
-                            <CardMedia image={ProfessorImage} style={styles.cardMedia}/>
-                        </Card>
+
+                    {/*> Professor card pair*/}
+                    <Grid
+                        container
+                        spacing={3}
+                        direction={"row"}
+                        alignItems={"center"}
+                        justify={"center"}
+                    >
+                        <Grid item xs={3}>
+                            <ReactCardFlip isFlipped={this.state.isProfessorFlipped}>
+                                <Card>
+                                    <CardActions style={styles.professorCardAction}>
+                                        <Button variant={"outlined"} onClick={this.flipProfessorCard}>Professor</Button>
+                                    </CardActions>
+                                </Card>
+                                <Card>
+                                    <Grid container
+                                          direction={"column"}>
+                                        <TextField required label="Login"/>
+                                        <TextField required label="Password" type={"password"}/>
+                                        <TextField required label="Repeat password" type={"password"}/>
+                                        <Button>Register</Button>
+                                    </Grid>
+                                </Card>
+                            </ReactCardFlip>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Card>
+                                <CardMedia image={ProfessorImage} style={styles.cardMedia}/>
+                            </Card>
+                        </Grid>
+                    </Grid>
+
+                    {/*> Student card pair*/}
+                    <Grid
+                        container
+                        spacing={3}
+                        direction={"row-reverse"}
+                        alignItems={"center"}
+                        justify={"center"}
+                    >
+                        <Grid item xs={3}>
+                            <ReactCardFlip isFlipped={this.state.isStudentFlipped}>
+                                <Card>
+                                    <CardActions style={styles.studentCardAction}>
+                                        <Button variant={"outlined"} onClick={this.flipStudentCard}>Student</Button>
+                                    </CardActions>
+                                </Card>
+                                <Card>
+                                    <Grid container
+                                          direction={"column"}>
+                                        <TextField required label="Login"/>
+                                        <TextField required label="Password" type={"password"}/>
+                                        <TextField required label="Repeat password" type={"password"}/>
+                                        <Button>Register</Button>
+                                    </Grid>
+                                </Card>
+                            </ReactCardFlip>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Card>
+                                <CardMedia image={StudentImage} style={styles.cardMedia}/>
+                            </Card>
+                        </Grid>
+
                     </Grid>
                 </Grid>
-
-                {/*> Student card pair*/}
-                <Grid
-                    container
-                    spacing={3}
-                    direction={"row-reverse"}
-                    alignItems={"center"}
-                    justify={"center"}
-                >
-                    <Grid item xs={3}>
-                        <Card>
-                            <CardActions style={styles.studentCardAction}>
-                                <Button variant={"outlined"}>Student</Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Card>
-                            <CardMedia image={StudentImage} style={styles.cardMedia}/>
-                        </Card>
-                    </Grid>
-                </Grid>
-            </Grid>
-        );
+            );
+        }
 }
-
-function handleClick(e) {
-    e.preventDefault();
-    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
-}
-
 export default Registration;
