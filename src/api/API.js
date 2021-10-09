5c3ad3712206ffddf199cfd9af9f195481ca5608
Regyl <novikovevgeny1@gmail.com>
2021-10-09 20:16:24 +0300

    defeat CORS policy

--- src/api/API.js
+++ src/api/API.js
@@ -2,6 +2,8 @@ import * as axios from "axios";
 
 const BASE_URL = "http://localhost:8080";
 
+
+
 const instance = axios.create({
     baseURL: BASE_URL,
     method: "GET",
@@ -15,11 +17,33 @@ const headerInstance = axios.create({
     method: "POST",
     headers: {
         "Content-Type": "application/json",
+
     },
 });
 
+const login = axios.create({
+    baseURL: BASE_URL,
+    method: "POST",
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/x-www-form-urlencoded",
+    },
+});
+
+let cookie;
+
+
 export const API = {
+    setCookie(value) {
+        cookie=value;
+    },
     postNewUser(user) {
         return headerInstance.post("/register", user);
+    },
+    loginIn(user) {
+        return login.post('/sign-in', user);
+    },
+    getStudents() {
+        return headerInstance.get('/students').header("Cookie", cookie);
     }
 }
--- src/components/authorisation/Authorisation.jsx
+++ src/components/authorisation/Authorisation.jsx
@@ -1,15 +1,57 @@
 import React from 'react';
-import {Box} from "@material-ui/core";
+import {Box, Button, Card, Grid, TextField} from "@material-ui/core";
+import {API} from "../../api/API";
+import axios from "axios";
 
 
-const Authorisation = (props) => {
+class Authorisation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            login: '',
+            password: '',
+        }
+        this.handleChangeLogin = this.handleChangeLogin.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.onLoginClick = this.onLoginClick.bind(this);
+    }
+    handleChangeLogin(e) {
+        this.setState({login: e.target.value});
+    }
+    handleChangePassword(e) {
+        this.setState({password: e.target.value});
+    }
+    onLoginClick(e) {
+        let user = 'username='+this.state.login+'&password='+this.state.password;
+        axios.post('http://localhost:8080/sign-in', user, {withCredentials: true});
+        let resp = axios.get('http://localhost:8080/tests/', {withCredentials: true}).then((response) => {console.log(response)});
+        /*let response = API.loginIn(user)
+            .then((response) => {
+                console.log(response.getHeader('Set-Cookie') + '1');
+            })
+            .catch((err) => {
+                console.log(err.status + 'err')
+            });*/
+    }
 
+    render() {
 
-    return (
-        <Box>
-            Auth
-        </Box>
-    );
+        return (
+            <Grid container
+                  direction={"column"}
+                  spacing={2}
+                  justifyContent={"center"}
+            >
+                <Grid item xs={3}>
+                    <Card>
+                        <TextField required label="Login" onChange={this.handleChangeLogin} id={"Administrator"} value={this.state.login}/>
+                        <TextField required label="Password" type={"password"} onChange={this.handleChangePassword} value={this.state.password}/>
+                        <Button variant={"outlined"} onClick={this.onLoginClick}>Sign in</Button>
+                    </Card>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default Authorisation;
